391f351e908e44daba550d149207ed849b3d8139
tarpan96 <71849744+tarpan96@users.noreply.github.com>
2020-11-12 22:25:22 -0500

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,17 @@
+Tarpan Patel
+Tarpan_patel@student.uml.edu
+91.61 GUI Programming I Assignment: Jquery
+Tarpan Patel Umass Lowell Computer Science, tarpan_patel@student.uml.edu
+Copyright (c) 2013 by Tarpan Patel. All rights reserved.
+11/12/20
+
+
+In this assignment, we extend previous assignment that focused on javascript to create table but use
+jquery for validate user input
+
+
+Sources: 
+https://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
+https://www.valentinog.com/blog/html-table/
+https://getbootstrap.com/docs/4.0/components/*
+http://w3schools.com
---
+++ js/validate.js
@@ -0,0 +1,93 @@
+/*
+Tarpan Patel
+91.61 GUI Programming I Assignment: Jquery
+Tarpan Patel Umass Lowell Computer Science, tarpan_patel@student.uml.edu
+Copyright (c) 2013 by Tarpan Patel. All rights reserved.
+11/12/20
+*/   
+
+$(document).ready(function() {
+
+    // Check if inputs are a whole number.
+    $.validator.addMethod("validInt", function(value){
+        if((value % 1) == 0){
+            return true;
+        }else {
+            return false;
+        };
+    }, "Please enter a whole number");
+
+    //Check if each input meets the requirements
+    $('#inputForm').validate ( {
+        rules: {
+            minCol: {
+                required: true,
+                number: true,
+                min: -50,
+                max: 50,
+                validInt: true
+            },
+            maxCol: {
+                required: true,
+                number: true,
+                min: -50,
+                max: 50,
+                validInt: true
+            },
+
+            minRow: {
+                required: true,
+                number: true,
+                min: -50,
+                max: 50,
+                validInt: true
+            },
+
+            maxRow: {
+                required: true,
+                number: true,
+                min: -50,
+                max: 50,
+                validInt: true
+            }
+        },
+        //Based on requirement needed, get corresponding error message
+        messages:  {
+            minCol: {
+                required: "No value entered. Please enter a value",
+                number: "Input is a number, please enter only numbers",
+                min: "Out of Range: Please enter a value greater or equal to -50",
+                max: "Out of Range: Please enter a value less or equal to 50"
+            },
+           
+           maxCol: {
+                required: "No value entered. Please enter a value",
+                number: "Input is a number, please enter only numbers",
+                min: "Out of Range: Please enter a value greater or equal to -50",
+                max: "Out of Range: Please enter a value less or equal to 50"
+            },
+            minRow: {
+                required: "No value entered. Please enter a value",
+                number: "Input is a number, please enter only numbers",
+                min: "Out of Range: Please enter a value greater or equal to -50",
+                max: "Out of Range: Please enter a value less or equal to 50"
+            },
+            maxRow: {
+                required: "No value entered. Please enter a value",
+                number: "Input is not a number, please enter only numbers",
+                min: "Out of Range: Please enter a value greater or equal to -50",
+                max: "Out of Range: Please enter a value less or equal to 50"
+            }
+        },
+        //errorPlacement print the error message
+        errorPlacement: function(errorMessage, element) {
+            
+            errorMessage.insertAfter(element);
+        },
+
+        //call getInfo
+        submitHandler: function() {
+          setInfo();
+        }
+    });
+});
